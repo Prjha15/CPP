14c022da7c93431c587095a1ddc0159d46dbc2c4
Priyanka Jha <priyankajha1516@gmail.com>
2025-09-07 22:18:46 +0530

    Add files via upload

---
+++ arithmatic_operations.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a,b;
+cout<<"ENTER THE VALUES OF a AND b"<<endl;
+cin>>a;
+cin>>b;
+cout<<"SUM IS "<<a+b<<endl;
+cout<<"DIFFERENCE IS "<<a-b<<endl;
+cout<<"PRODUCT IS "<<a*b<<endl;
+cout<<"DIVISION IS "<<a/b<<endl;
+return 0;
+}
+ 
