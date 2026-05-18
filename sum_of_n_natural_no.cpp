028919f0fbd7bc0e1f2bc2aa4092d0474e3f380a
Prjha15 <priyankajha1516@gmail.com>
2026-05-18 12:45:21 +0530

    new

--- sum_of_n_natural_no.cpp
+++ sum_of_n_natural_no.cpp
@@ -1,8 +1,15 @@
 #include<iostream>
 using namespace std;
 float sum(int n){
-
+    int addition=n*(n+1)/2;
+    return addition;
 }
 int main(){
-    
+    int a;
+    int result;
+    cout<<"ENTER THE NATURAL NUMBER";
+    cin>>a;
+    result=sum(a);
+    cout<<"Addition of provided natural numbers are "<<result;
+    return 0;
 }
