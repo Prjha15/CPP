7ee56d262802978dcb91f421d5fd79c848df2b61
Prjha15 <priyankajha1516@gmail.com>
2026-05-19 15:59:28 +0530

    inverted half pyramid triangle

---
+++ pattern4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,i,j;
+    cout<<"ENTER THE VALUE OF n:";
+    cin>>n;
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n-i+1;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
