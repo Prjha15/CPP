d27675ce6d20f66cff1b90c58dbcd9fd53cbd43b
Prjha15 <priyankajha1516@gmail.com>
2026-05-19 16:31:09 +0530

    pyramid

---
+++ pattern5.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+using namespace std;
+int main(){
+    int n,i,j;
+    cout<<"ENTER THE VALUE OF n:";
+    cin>>n;
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n-i;j++){
+            cout<<" ";
+        }
+            for(j=1;j<=2*i-1;j++){
+                cout<<"*";
+            }
+            for(j=1;j<=n-1;j++){
+                cout<<" ";
+            }
+cout<<endl;
+    }
+    return 0;
+
+}
