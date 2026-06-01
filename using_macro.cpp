91a20de89bbf7cfadbd9a6c6b83ba827317997ff
Prjha15 <priyankajha1516@gmail.com>
2026-06-01 14:22:19 +0530

    object like macro

---
+++ using_macro.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+#define pi 3.14159
+int main(){
+    float r;
+    cout<<"ENTER THE VALUE OF r";
+    cin>>r;
+    float area=pi*pow(r,2);
+    cout<<"AREA IS "<<area;
+    return 0;
+}
